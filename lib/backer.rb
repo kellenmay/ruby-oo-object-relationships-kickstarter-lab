dd3341f0c43e12cad6f8eb01c34652005d0a9369
Kellen <kellenbmay@gmail.com>
2020-12-04 11:52:40 -0500

    commit tree 04c54c6e6a884b780089dcc794291d8671146f94

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        projects = ProjectBacker.all.select{|project_backer| project_backer.backer == self}
+        projects.map{|project_back| project_back.project}    
+    end
+
+end
